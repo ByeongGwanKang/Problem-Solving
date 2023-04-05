c3222ada48939b89f205fedc5380a0ca7a3d3cbb
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-04-05 15:28:47 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+	scanf("%d%d%d", &a, &b, &c);
+	int max = a;
+	if (b > max)
+		max = b;
+	if (c > max)
+		max = c;
+	if (a == b && a == c)
+		printf("%d", 10000 + a * 1000);
+	else if (a == b && a != c) {
+		printf("%d", 1000 + a * 100);
+	}
+	else if (a == c && a != b) {
+		printf("%d", 1000 + a * 100);
+	}
+	else if (b == c && a != b) {
+		printf("%d", 1000 + b * 100);
+	}
+	else if (a != b && a != c && b != c) {
+		printf("%d", max * 100);
+	}
+	return 0;
+}
