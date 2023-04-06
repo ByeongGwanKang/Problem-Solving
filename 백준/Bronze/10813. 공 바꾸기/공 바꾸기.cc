084c4d1e4f0854531138595070226214f5484fe7
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-04-06 17:34:27 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+int main(void) {
+	using namespace std;
+	int n, m, a, b, temp;
+	int arr[101] = { 0 };
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		arr[i] = i;
+	}
+	for (int i = 1; i <= m; i++) {
+		cin >> a >> b;
+		temp = arr[b];
+		arr[b] = arr[a];
+		arr[a] = temp;
+	}
+	for (int i = 1; i <= n; i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
