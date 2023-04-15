3c69bb71c049c978810098e26e040d635923b44d
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-04-15 20:54:07 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0); cin.tie(0); //c++입출력만 사용하기
+	int n, k;
+	cin >> n >> k;
+	vector <int>arr(n);
+	cout << "<";
+	int val = 0;
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) {
+		cnt = 0;
+		for (int j = 0; j < k; j++) {
+			if (arr[val] != 0) {
+				j--;
+			}
+			cnt = val;
+			val++;
+			if (val >= n) {
+				val %= n;
+			}
+			
+		}
+		arr[cnt]++;
+		if (i == n)
+			cout << cnt+1;
+		else
+		cout << cnt+1 << ", ";
+	}
+	cout << ">";
+	return 0;
+}
