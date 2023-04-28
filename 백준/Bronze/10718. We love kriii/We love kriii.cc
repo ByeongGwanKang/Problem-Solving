844362b7f1dcfa1fc87b8281d809b1dd4362e1c2
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-04-28 20:18:19 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include<stack>
+using namespace std;
+int main(void) {
+	ios::sync_with_stdio(0); cin.tie(0); //c++입출력만 사용하기
+	cout << "강한친구 대한육군\n강한친구 대한육군";
+	return 0;
+}
