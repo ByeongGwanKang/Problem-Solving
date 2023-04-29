367bb2bfbd7f4d165d5c88abb14516894483aa9c
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-04-29 14:05:18 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include<stack>
+using namespace std;
+int main(void) {
+	ios::sync_with_stdio(0); cin.tie(0); //c++입출력만 사용하기
+	cout << ".  .   .\n" << "|  | _ | _. _ ._ _  _\n" << 
+		"|/\\|(/.|(_.(_)[ | )(/.";
+	return 0;
+}
