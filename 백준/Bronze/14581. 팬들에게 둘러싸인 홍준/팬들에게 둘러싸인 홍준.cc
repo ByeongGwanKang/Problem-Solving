86e8b0b4d621b2d9d3c8096c70d908174b646e80
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-05-04 22:45:31 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include<stack>
+using namespace std;
+int main(void) {
+	ios::sync_with_stdio(0); cin.tie(0); //c++입출력만 사용하기
+	string str;
+	cin >> str;
+	cout << ":fan::fan::fan:" << endl;
+	cout << ":fan::" << str << "::fan:" << endl;
+	cout << ":fan::fan::fan:";
+
+	return 0;
+}
