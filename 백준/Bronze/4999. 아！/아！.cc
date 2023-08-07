cc5aa0346e87bae191414528fd19cdb36289da30
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-08-07 22:19:34 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main(void){
+	ios::sync_with_stdio(0); 
+	cin.tie(0);
+	string a,b;
+	getline(cin,a);
+	getline(cin,b);
+	if(a.length()>=b.length()){
+		cout<<"go";
+	}
+	else cout<<"no";
+
+	return 0;
+}
