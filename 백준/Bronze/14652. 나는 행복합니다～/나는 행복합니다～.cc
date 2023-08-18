049cfaaca32d29ed33cdfee6a6c6f82645c8653d
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-08-18 20:27:19 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n,m,k;
+	cin>>n>>m>>k;
+	cout<<k/m<<" "<<k%m;
+	return 0;
+}
