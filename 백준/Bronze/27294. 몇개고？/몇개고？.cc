bc0e5304098a302d29a3a75f008cd4fe93e96118
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-08-26 09:48:50 +0900

    [Bronze V] Title: 몇개고?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27294. 몇개고？/몇개고？.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t,s;
+	cin>>t>>s;
+	if(t<=11||t>16||s==1){
+		cout<<280;
+	}
+	else cout<<320;
+}
