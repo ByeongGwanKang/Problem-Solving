b2b512baa0a29dc03b15a06cccf9959ed9e76f13
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-08-27 07:51:54 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int l,p;
+	cin>>l>>p;
+	int a1,a2,a3,a4,a5;
+	cin>>a1>>a2>>a3>>a4>>a5;
+	cout<<a1-l*p<<" "<<a2-l*p<<" "<<a3-l*p<<" "<<a4-l*p<<" "<<a5-l*p;
+	}
