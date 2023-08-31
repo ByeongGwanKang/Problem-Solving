dab5c5f546e02ef72d5ea93443e4c0ae0ebc72da
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-08-31 11:58:58 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	while(true){
+		int a,b;
+		cin>>a>>b;
+		if(a==0&&b==0)
+			return 0;
+		cout<<a+b<<"\n";
+	}
+	return 0;
+}
