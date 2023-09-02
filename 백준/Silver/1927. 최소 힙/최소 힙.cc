9204df9ba98fc362f337d19e1ee82437751cfb78
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-09-02 16:19:18 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	priority_queue<int,vector<int>,greater<int>> q;
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		int val;
+		cin>>val;
+		if(val!=0)
+			q.push(val);
+		else{
+			if(q.empty())
+				cout<<0<<"\n";
+			else{
+			cout<<q.top()<<"\n";
+			q.pop();
+			}
+		}
+	}
+}
