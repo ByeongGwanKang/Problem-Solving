beae848305633ddb6c1267ba8023f8fd74a6c847
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-09-06 12:20:20 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin>>n;
+	vector<int>arr(n+1);
+	vector<int>ans(n+1);
+	for(int i=1;i<=n;i++){
+		cin>>arr[i];
+	}
+	ans[0]=0;
+	ans[1]=arr[1];
+	ans[2]=ans[1]+arr[2];
+	for(int i=3;i<=n;i++){
+		ans[i]=max(max(ans[i-2]+arr[i],ans[i-3]+arr[i-1]+arr[i]),ans[i-1]);
+	}
+	cout<<ans[n];
+	return 0;
+}
