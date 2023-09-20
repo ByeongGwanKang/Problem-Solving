2c07b92a5bef7ddf4c172fcf2b87258b50177c67
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-09-21 07:26:42 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int w,h;
+	cin>>w>>h;
+	cout<<fixed;
+	cout.precision(1);
+	cout<<(double)w*h/2;
+    return 0;
+}
