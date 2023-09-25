a9da57eb0bd0808066298d188f50e58615500cd9
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-09-25 09:44:17 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string str;
+	cin>>str;
+	if(str=="SONGDO"){
+		cout<<"HIGHSCHOOL";
+	}
+	if(str=="CODE"){
+		cout<<"MASTER";
+	}
+	if(str=="2023"){
+		cout<<"0611";
+	}
+	if(str=="ALGORITHM"){
+		cout<<"CONTEST";
+	}
+	return 0;
+}
