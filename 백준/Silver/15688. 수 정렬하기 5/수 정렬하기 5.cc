591074fc71d749e2e8c037d381e1135cf8e76ae0
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-10-09 09:53:31 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4748 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin>>n;
+	vector<int>v;
+	for(int i=0;i<n;i++){
+		int val;
+		cin>>val;
+		v.push_back(val);
+	}
+	sort(v.begin(),v.end());
+	for(int i:v){
+		cout<<i<<"\n";
+	}
+	return 0;
+}
