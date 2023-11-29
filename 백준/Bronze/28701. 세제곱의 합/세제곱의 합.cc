e5967735b56710cee9d6dd75f750849aa8678a92
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-11-29 12:35:36 +0900

    [Bronze V] Title: 세제곱의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n;
+	cin>>n;
+	int val=n*(n+1)/2;
+	cout<<val<<"\n";
+	cout<<val*val<<"\n"<<val*val;
+	return 0;
+}
