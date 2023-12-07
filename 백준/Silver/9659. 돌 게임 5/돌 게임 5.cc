e0b0931fb93c9fd657c4201e624abced1c2f58be
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-12-07 12:11:08 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	long long n;
+	cin>>n;
+	if(n%2==0){
+		cout<<"CY";
+	}
+	else{
+		cout<<"SK";
+	}
+	return 0;
+}
