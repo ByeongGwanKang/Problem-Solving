0c00a312fa143fbd11d9f268b777a63bd105f4c5
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-12-09 07:50:35 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cout<<fixed;
+	cout.precision(6);
+	int n;
+	cin>>n;
+	double PI=3.14159265359;
+	cout<<n*n*PI<<"\n"<<n*n*2.0;
+	return 0;
+}
