d6b7973240fb2335a5018fb38007a9d8419c492c
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2023-12-08 12:24:19 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int b;
+	cin>>b;
+	cout<<b/11*10;
+	return 0;
+}
