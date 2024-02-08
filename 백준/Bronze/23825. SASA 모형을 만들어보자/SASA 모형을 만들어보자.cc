805b2a7b90b8875f48471112a426d604f2a7e47c
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-02-08 12:21:31 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n,m;
+	cin>>n>>m;
+	cout<<min(n/2,m/2);
+	return 0;
+}
