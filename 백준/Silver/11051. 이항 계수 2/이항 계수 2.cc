8f1bab13e238f21949d5109c0d4518b05b35b210
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-02-09 21:13:39 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5844 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -13,7 +13,7 @@ int main(void){
 		arr[i][i]=1;	//i개중 i개 선택
 		arr[i][1]=i;	//i개중 1개 선택
 	}
-	for(int i=2;i<=n;i++){
+	for(int i=1;i<=n;i++){
 		for(int j=2;j<=i;j++){
 			arr[i][j]=arr[i-1][j]+arr[i-1][j-1];
 			arr[i][j]%=10007;
