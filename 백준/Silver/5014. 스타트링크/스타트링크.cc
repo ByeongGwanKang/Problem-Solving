75903b5e35ba0402e3d6edb394d4cce80fbfa6c0
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-02-12 14:31:58 +0900

    [Silver I] Title: 스타트링크, Time: 16 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+int f,s,g,u,d;
+vector<bool>vis;
+queue<pair<int,int>>q;
+bool ansflag=false;
+void bfs(){
+	while(!q.empty()){
+		int now=q.front().first;
+		int cnt=q.front().second;
+		q.pop();
+		if(now==g){
+			cout<<cnt;
+			ansflag=true;
+			break;
+		}
+		int up=now+u;
+		int down=now-d;
+		if(up<=f&&vis[up]==false){
+			q.push({up,cnt+1});
+			vis[up]=true;
+		}
+		if(down>0&&vis[down]==false){
+			q.push({down,cnt+1});
+			vis[down]=true;
+		}
+	}
+}
+int main(void){
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin>>f>>s>>g>>u>>d;
+	vis.resize(f+1);
+	vis[s]=true;
+	q.push({s,0});
+	bfs();
+	if(!ansflag){
+		cout<<"use the stairs";
+	}
+	return 0;
+}
