fcfb473bc67c6d80f1256ef5c4b1779ab32cb31e
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-02-26 12:23:09 +0900

    [Silver V] Title: 젓가락, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24228. 젓가락/젓가락.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  long long n,r;
+  cin>>n>>r;
+  cout<<r*2+n-1;
+  return 0;
+}
