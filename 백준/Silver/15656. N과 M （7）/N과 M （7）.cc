6b2a59c98b60d10c44f45965af5889b4b0a1130e
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-03-25 10:55:16 +0900

    [Silver III] Title: N과 M (7), Time: 480 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int q[9];
+vector<int> v;
+void recursive(int cnt) {
+  if (cnt == m) {
+    for (int i = 0; i < m; i++) {
+      cout << q[i] << " ";
+    }
+    cout << "\n";
+    return;
+  }
+  for (int i = 0; i < n; i++) {
+    q[cnt] = v[i];
+    recursive(cnt + 1);
+  }
+}
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cin >> n >> m;
+  for (int i = 0; i < n; i++) {
+    int val;
+    cin >> val;
+    v.push_back(val);
+  }
+  sort(v.begin(), v.end());
+  recursive(0);
+  return 0;
+}
