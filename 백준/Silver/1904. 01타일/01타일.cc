84287e98bb5cfd7306cc4728b701fc4bff667993
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-04-16 10:36:08 +0900

    [Silver III] Title: 01타일, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n;
+  cin >> n;
+  vector<int> v(n + 1);
+  v[1] = 1;
+  v[2] = 2;
+  for (int i = 3; i <= n; i++) {
+    v[i] = v[i - 2] + v[i - 1];
+    v[i] %= 15746;
+  }
+  cout << v[n];
+  return 0;
+}
