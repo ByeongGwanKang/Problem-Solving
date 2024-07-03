9c8c0cec87522de76d90eeb3bbffd25296cfbd00
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-07-03 11:54:48 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int n;
+  cin >> n;
+  while (n--) {
+    int k;
+    cin >> k;
+    cout << 1 << "\n";
+  }
+  return 0;
+}
