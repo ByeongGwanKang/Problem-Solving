89cc31ef33db58adf8d56c04aa3537d5245e0e98
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-10-09 11:01:36 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << 3 * b / a * c;
+  return 0;
+}
