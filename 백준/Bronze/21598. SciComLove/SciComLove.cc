518a2a2c6272038daf6b316d3971f2e563f4635b
ByeongGwan Kang <100698402+ByeongGwanKang@users.noreply.github.com>
2024-11-17 09:21:56 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+
+  ios::sync_with_stdio(false);
+
+  cin.tie(NULL);
+
+  cout.tie(NULL);
+
+  int n;
+
+  cin >> n;
+
+  for (int i = 0; i < n; i++) {
+
+    cout << "SciComLove\n";
+
+  }
+
+  return 0;
+
+}
